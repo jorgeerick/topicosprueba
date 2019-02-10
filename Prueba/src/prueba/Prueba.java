182c1470e771b912579358b665d92c45ad718012
jorgeerick <47402995+jorgeerick@users.noreply.github.com>
2019-02-10 17:52:10 -0500

    Update Prueba.java

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -17,6 +17,7 @@ public class Prueba {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("chupelo erik");
+        System.out.println("Erick y Gabriel");
     }
     
 }
